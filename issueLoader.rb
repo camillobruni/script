d52d058afe718b6d6d427d1a3e71819a18680c90
Camillo Bruni <camillobruni@gmail.com>
2011-09-25 06:13:30 +0200

    cleaned up script

--- issueLoader.rb
+++ issueLoader.rb
@@ -1,36 +1,14 @@
 #!/usr/bin/env ruby
 
-`date`
-
 require 'fileutils'
+
+# ============================================================================
 
 def help
     $stderr.puts "    usage /.image issueNumber"
     $stderr.puts "          --hack     edit the sources of this script"
     $stderr.puts "          -h/--help  show this help text"
 end
-
-if $*.size != 1
-    help
-    exit 1
-end
-
-issueNumber = $*[0]
-
-# ===========================================================================
-
-version  = '1.4'
-tmp      = `mktemp -d -t pharo`.chomp
-
-imageUrl = "https://ci.lille.inria.fr/pharo/view/Pharo%20#{version}/job/Pharo%20#{version}/lastSuccessfulBuild/artifact/Pharo-#{version}.zip"
-artifact = "Pharo#{version}"
-name = "Pharo-#{version}"
-path = "/Users/benjamin/Images/Monkeys"
-destination = "#{path}/Monkey#{issueNumber}"
-vmPath = "/Users/benjamin/Images/StackVM.app/Contents/MacOS/StackVM"
-
-# ===========================================================================
-
 
 def editor()
     if ENV['EDITOR']
@@ -48,20 +26,40 @@ elsif $*[0] == "--hack"
     exec("#{editor()} #{sourceFile}")
 end
 
+if $*.size != 1
+    help
+    exit 1
+end
 
 
+# ===========================================================================
 
+issueNumber = $*[0]
+version  = '1.4'
+tmp      = `mktemp -d -t pharo`.chomp
+
+imageUrl = "https://ci.lille.inria.fr/pharo/view/Pharo%20#{version}/job/Pharo%20#{version}/lastSuccessfulBuild/artifact/Pharo-#{version}.zip"
+artifact = "Pharo#{version}"
+name = "Pharo-#{version}"
+destination = "Monkey#{issueNumber}"
+
+# ============================================================================
 
 puts "fetching the latest image"
-
-`cd "#{path}" && wget --tries=2 --timeout=3 --no-check-certificate "#{imageUrl}" --output-document="artifact#{issueNumber}.zip" && cp "#{path}/artifact#{issueNumber}.zip" "#{path}/backup.zip"  || cp "#{path}/backup.zip" "#{path}/artifact#{issueNumber}.zip"`
+`wget --tries=2 --timeout=3 --no-check-certificate "#{imageUrl}" --output-document="artifact#{issueNumber}.zip" \
+    && cp "artifact#{issueNumber}.zip" "/backup.zip"  \
+    || cp "backup.zip" "artifact#{issueNumber}.zip"`
 
 puts "Unzipping the archive"
-`unzip -x "#{path}/artifact#{issueNumber}.zip" -d "#{destination}"`
-`cd "#{destination}" && mv "#{name}/#{name}.image" "Monkey#{issueNumber}.image" ; mv "#{name}/#{name}.changes" "Monkey#{issueNumber}.changes" ; mv "#{name}/PharoV10.sources" PharoV10.sources`
+`unzip -x "artifact#{issueNumber}.zip" -d "#{destination}"`
+`cd "#{destination}" && ( \
+    mv "#{name}/#{name}.image" "Monkey#{issueNumber}.image"; \ 
+    mv "#{name}/#{name}.changes" "Monkey#{issueNumber}.changes"; \ 
+    mv "#{name}/PharoV10.sources" PharoV10.sources \ 
+)`
 
 puts "Cleanup"
-`rm "#{path}/artifact#{issueNumber}.zip"`
+`rm "artifact#{issueNumber}.zip"`
 `rm -rf "#{tmp}"`
 
 # ===========================================================================
@@ -98,9 +96,9 @@ Smalltalk snapshot: false andQuit: true.
 
 IDENTIFIER
 }
+
 puts "Open the image and check the issue number #{issueNumber}"
 `"#{vmPath}" "#{destination}/Monkey#{issueNumber}.image" "#{destination}/issueLoading.st"`
 
 puts "Remove the folder #{destination}"
 `rm -R "#{destination}"`
-`date`
