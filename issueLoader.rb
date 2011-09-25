f14736eed0e254d7c28e94db4dafede81c017259
Camillo Bruni <camillobruni@gmail.com>
2011-09-25 08:41:28 +0200

    extendede the issueLoader script with a timeout limit

--- issueLoader.rb
+++ issueLoader.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
+require 'timeout'
 
 # ============================================================================
 
@@ -32,6 +33,10 @@ if $*.size != 1
 end
 
 
+def guard()
+    exit $?.to_i if !$?.success?
+end
+
 # ===========================================================================
 
 issueNumber = $*[0]
@@ -45,28 +50,35 @@ destination = "Monkey#{issueNumber}"
 
 # ============================================================================
 
+
 puts "fetching the latest image"
-`wget --tries=2 --timeout=3 --no-check-certificate "#{imageUrl}" --output-document="artifact#{issueNumber}.zip" \
+`wget --tries=1 --timeout=3 --no-check-certificate "#{imageUrl}" --output-document="artifact#{issueNumber}.zip" \
     && cp "artifact#{issueNumber}.zip" "/backup.zip"  \
     || cp "backup.zip" "artifact#{issueNumber}.zip"`
+guard()
 
 puts "Unzipping the archive"
 `unzip -x "artifact#{issueNumber}.zip" -d "#{destination}"`
-`cd "#{destination}" && ( \
-    mv "#{name}/#{name}.image" "Monkey#{issueNumber}.image"; \ 
-    mv "#{name}/#{name}.changes" "Monkey#{issueNumber}.changes"; \ 
-    mv "#{name}/PharoV10.sources" PharoV10.sources \ 
-)`
+Dir::chdir(destination)
+guard()
+
+imagePath = `find . -name "*.image"`.chomp
+imagePath = imagePath.chomp(File.extname(imagePath))
+FileUtils.move(imagePath+'.image', "Monkey#{issueNumber}.image")
+FileUtils.move(imagePath+'.changes', "Monkey#{issueNumber}.changes")
+
+if File.exists? File.dirname(imagePath)+"/PharoV10.sources"
+    FileUtils.move(File.dirname(imagePath)+"/PharoV10.sources", "PharoV10.sources")
+end
 
 puts "Cleanup"
-`rm "artifact#{issueNumber}.zip"`
-`rm -rf "#{tmp}"`
+`rm "../artifact#{issueNumber}.zip" && rm -rf "#{tmp}"`
+guard()
 
 # ===========================================================================
 
-File.open("#{destination}/issueLoading.st", 'w') {|f| 
+File.open("issueLoading.st", 'w') {|f| 
     f.puts <<IDENTIFIER
-
 | tracker issue |
 
 World submorphs do: [:each | each delete ].
@@ -84,21 +96,48 @@ Gofer new
 	
 (Smalltalk at: #ConfigurationOfContinousIntegration) perform: #loadDefault.
 
+"===================================="
 tracker := GoogleIssueTracker pharo.
 tracker authenticate: 'pharo.ulysse@gmail.com' with: 'AydsInJis'.
 
-
 "===================================="
 issue := tracker issue: #{issueNumber}.
+Smalltalk at: 'MonkeyIssue' put: issue.
+Smalltalk snapshot: true andQuit: false.
+"===================================="
+
 issue loadAndTest.
 
+"===================================="
 Smalltalk snapshot: false andQuit: true.
 
 IDENTIFIER
 }
 
-puts "Open the image and check the issue number #{issueNumber}"
-`"#{vmPath}" "#{destination}/Monkey#{issueNumber}.image" "#{destination}/issueLoading.st"`
+pid = 0
+begin
+    #kill the build process after 15 minutes
+    puts "Open the image and check the issue number #{issueNumber}"
+    timeout(80) {
+        pid = Process.spawn("pharo '#{Dir.pwd}/Monkey#{issueNumber}.image' '#{Dir.pwd}/issueLoading.st'")
+        puts pid
+        Process.wait
+    }
+rescue Timeout::Error    
+    puts pid
+    Process.kill('KILL', pid)
+    Process.kill('KILL', pid+1) #this is pure guess...
+    puts "Loading #{issueNumber} took longer than 15mins"
+    File.open("issueLoading.st", 'w') {|f| 
+        f.puts <<IDENTIFIER
+            (Smalltalk at: 'MonkeyIssue') 
+                reviewNeeded: 'Timeout occured while loading and testing the code'.
+            Smalltalk snapshot: false andQuit: true.
+IDENTIFIER
+    }
+    `pharo "#{Dir.pwd}/Monkey#{issueNumber}.image" "#{Dir.pwd}/issueLoading.st"`
+end
+
 
 puts "Remove the folder #{destination}"
 `rm -R "#{destination}"`
