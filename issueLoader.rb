4daffe43f9fb6f34bddc3315eac1843d1b457253
Camillo Bruni <camillobruni@gmail.com>
2011-11-21 12:38:28 +0100

    made fancier issueLoader

--- issueLoader.rb
+++ issueLoader.rb
@@ -54,10 +54,49 @@ end
 
 # ============================================================================
 
+def colorize(text, color_code)
+  "\033#{color_code}#{text}\033[0m"
+end
+
+def red(text)
+    colorize(text, "[31m")
+end
+
+def green(text)
+    colorize(text, "[32m")
+end
+
+def yellow(text)
+    colorize(text, "[33m")
+end
+
+# ============================================================================
+
+if File.exists? destination
+    puts red("Issue has been loaded before #{Dir.pwd}/#{destination}")
+    while true
+        print 'exit[e], reuse[r] or delete[D] files: '
+        result = $stdin.gets.downcase.chomp
+        break if ['e', 'r', 'd'].include? result
+        break if result.empty?
+    end
+
+    case result
+    when 'e'
+        exit 0
+    when 'r'    
+        puts red('resuse not yet implemented')
+        exit 1
+    else
+        `rm -rf #{destination}`
+    end
+end
+
+# ============================================================================
 
 if updateImage
-    puts "fetching the latest image"
-        `wget --tries=1 --timeout=3 --no-check-certificate "#{imageUrl}" --output-document="artifact#{issueNumber}.zip" \
+    puts yellow("Fetching the latest image")
+        `curl --progress-bar -o "artifact#{issueNumber}.zip" "#{imageUrl}" \
         && cp "artifact#{issueNumber}.zip" "backup.zip"  \
         || cp "backup.zip" "artifact#{issueNumber}.zip"`
 else
@@ -65,7 +104,10 @@ else
 end
 guard()
 
-puts "Unzipping the archive"
+
+# ============================================================================
+
+puts yellow("Unzipping the archive")
 `unzip -x "artifact#{issueNumber}.zip" -d "#{destination}" && rm -rf "#{destination}/__MACOSX"`
 Dir::chdir(destination)
 guard()
@@ -79,14 +121,20 @@ if File.exists? File.dirname(imagePath)+"/PharoV10.sources"
     FileUtils.move(File.dirname(imagePath)+"/PharoV10.sources", "PharoV10.sources")
 end
 
-puts "Cleanup"
+# ============================================================================
+#
+puts yellow("Cleaing up unzipped files")
 `rm "../artifact#{issueNumber}.zip" && rm -rf "#{tmp}"`
 guard()
 
 # ===========================================================================
 
-exit 1 if !system("ping -c 1 ss3.gemstone.com")
+if !system("ping -c 1 ss3.gemstone.com > /dev/null")
+    puts red('Could not find ss3.gemstone.com')
+    exit 1
+end
 
+# ===========================================================================
 File.open("issueLoading.st", 'w') {|f| 
 f.puts <<IDENTIFIER
 | tracker issue |
@@ -132,18 +180,16 @@ IDENTIFIER
 pid = 0
 begin
     #kill the build process after 1 hour
-    puts "Opening the image and checking  issue ##{issueNumber}"
+    puts yellow("Opening the image for issue ##{issueNumber}")
     puts "    http://code.google.com/p/pharo/issues/detail?id=#{issueNumber}"
     timeout(60 * 60) {
         pid = Process.spawn("stackVM '#{Dir.pwd}/Monkey#{issueNumber}.image' '#{Dir.pwd}/issueLoading.st'")
-        puts pid
         Process.wait
     }
 rescue Timeout::Error    
-    puts pid
     Process.kill('KILL', pid)
     Process.kill('KILL', pid+1) #this is pure guess...
-    puts "Loading #{issueNumber} took longer than 15mins"
+    puts red("Loading #{issueNumber} took longer than 15mins")
     File.open("issueLoading.st", 'w') {|f| 
         f.puts <<IDENTIFIER
 "===================================="
@@ -160,7 +206,21 @@ IDENTIFIER
 end
 
 
-puts "Remove the folder #{destination}"
-`rm -R "#{destination}"`
-`date`
+# ===========================================================================
+
+while true
+    print "Remove the folder #{Dir.pwd} [yN]?"
+    result = $stdin.gets.downcase.chomp
+    break if ['y', 'N',].include? result
+    break if result.empty?
+end
+
+case result
+when 'y'
+    `rm -R "#{Dir.pwd}"`
+else
+    `open #{Dir.pwd}`
+end
+
+puts `date`
 `open "http://code.google.com/p/pharo/issues/detail?id=#{issueNumber}"`
