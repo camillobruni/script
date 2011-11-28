4ba96d8433798b8c1eb9e6f1c4b1f32c184a79f1
Camillo Bruni <camillobruni@gmail.com>
2011-11-28 21:22:55 +0100

    added new todo script updated scriptTemplate

--- todo.rb
+++ todo.rb
@@ -11,6 +11,7 @@
 
 require 'optparse' 
 require 'ostruct'
+require 'date'
 require 'logger'  
 
 
@@ -31,15 +32,15 @@ class App
 
     def run
         # Specify options
-        opts = OptionParser.new do |opts|
+        @opts = OptionParser.new do |opts|
             opts.program_name = 'todo'
-            opts.version = VERSION
+            opts.version = '0.1'
             opts.banner = 'todo [options] comment'
             
             opts.separator ""
             opts.separator "Specific options:"
             
-            #ADD CUSTOM OPTIONS HERE
+            #to be implemented
 
             opts.separator ""
             opts.separator "Common options:"
@@ -54,30 +55,48 @@ class App
                 exit
             end
 
-            opts.on_tail('--hack', "Edit the programs source") do
-                self.hack
-            end
+            opts.on_tail('--hack', "Edit the programs source") { self.hack }
             # TODO - add additional options
                   
         end
         
-        # print the help if we get no arguments
         if ARGV.empty?
-            puts opts
+            puts @opts
             exit
         end
 
-        opts.parse!(@arguments)
+        @opts.parse!(@arguments)
         self.process_command
     end
     
     protected
-    def process_command
-        # implement your custom command action here
+        def process_command
+            IO.popen('osascript', 'w') { |f|
+            f.puts <<EOF
+-- Variables
+set recipientAddress to "#{self.email}"
+set theSubject to "TODO: #{ARGV.join(' ')}"
+--Invoke Mail
+tell application "Mail"
+	set theMessage to make new outgoing message with properties {subject:theSubject, visible:true}
+	tell theMessage
+		make new to recipient with properties {address:recipientAddress}
+		--Send the Message
+		send
+	end tell
+end tell
+EOF
+        }
+        end
+
+    def email
+        return ENV['EMAIL'] if ENV['EMAIL']
+        @opts.warn 'Could not find $EMAIL ENV var'
+        exit -1
     end
 
-    # ========================================================================
-    def editor()
+
+    def editor
         if ENV['EDITOR']
             return ENV['EDITOR']
         else
@@ -87,12 +106,10 @@ class App
 
     def hack
         sourceFile = `readlink #{__FILE__} || echo #{__FILE__}`.chomp
-        exec(self.editor(), sourceFile)
+        exec(self.editor, sourceFile)
     end
 end
 
-
-# ============================================================================
 
 if __FILE__ == $0
     # preamble to change to the current scripts dir
@@ -109,5 +126,4 @@ if __FILE__ == $0
     app = App.new(ARGV, STDIN)
     app.run
 end
-
 
