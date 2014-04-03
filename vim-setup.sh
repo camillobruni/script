4db4ad008650c1cc8a4527ea8fe460d6b8a0df9b
Camillo Bruni <camillobruni@camillos-mbp.home>
2014-04-03 22:13:43 +0200

    new vim setup script

---
+++ vim-setup.sh
@@ -0,0 +1,26 @@
+#/usr/bin/bash
+
+set -e 
+
+# =============================================================================
+
+SOURCE_DIR=`readlink "$0"` || SOURCE_DIR="$0";
+SOURCE_DIR=`dirname "$DIR"`;
+cd "$DIR"
+SOURCE_DIR=`pwd -P`
+
+# =============================================================================
+if { [ "$1" = "-h" ] || [ "$1" = "--help" ]; }; then
+    echo "    Sets up the vim preferences in your home directory from the 
+    .vim directory in this repository:
+    $SOURCE_DIR/.vimrc -> ~/.vimrc
+    $SOURCE_DIR/.vim/ -> ~/.vim/"
+    exit 0
+fi
+
+# =============================================================================
+
+ln -vs $SOURCE_DIR/.vim $HOME/
+ln -vs $HOME/.vim/.vimrc $HOME/
+
+$HOME/vundle-install.sh
